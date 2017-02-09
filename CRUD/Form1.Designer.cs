d8ff93f291a645ef52d9b7516660690a22f09d13
Superkachek <xd.rashid@yandex.ru>
2017-02-09 19:14:58 +0600

    DZ5 Crud

---
+++ CRUD/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace CRUD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(0, 70);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(558, 340);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Имя";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Возраст";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Почта";
+            this.Column3.Name = "Column3";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(138, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 52);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Удалить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(28, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(104, 52);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Сохранить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(553, 24);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(553, 405);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+    }
+}
+
---
+++ CRUD/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int id = dataGridView1.SelectedCells[0].RowIndex;
+            dataGridView1.Rows.RemoveAt(id);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream(@"E:\DEC.txt", FileMode.Create);
+            StreamWriter streamWriter = new StreamWriter(fs);
+
+            try
+            {
+                for (int j = 0; j < dataGridView1.Rows.Count; j++)
+                {
+                    for (int i = 0; i < dataGridView1.Rows[j].Cells.Count; i++)
+                    {
+                        streamWriter.Write(dataGridView1.Rows[j].Cells[i].Value + "#");
+                    }
+
+                    streamWriter.Write("$");
+            }
+
+                streamWriter.Close();
+                fs.Close();
+
+                MessageBox.Show("Файл успешно сохранен");
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка при сохранении файла!");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FileStream fStream = new FileStream(@"E:\DEC.txt", FileMode.Open);
+            StreamReader streamReader = new StreamReader(fStream);
+
+            string[] str;
+            int numberOfRows = 2;
+           
+                string[] str1 = streamReader.ReadToEnd().Split('$');
+                numberOfRows = str1.Count();
+
+            dataGridView1.RowCount = numberOfRows;
+                for (int i = 0; i < numberOfRows - 1; i++)
+                {
+                    str = str1[i].Split('#');
+                    for (int j = 0; j < dataGridView1.ColumnCount; j++)
+                    {
+                        dataGridView1.Rows[i].Cells[j].Value = str[j];
+                    }
+                }
+            fStream.Close();
+            streamReader.Close();
+        }
+            
+        }
+
+      
+    }
+
+
+
+
